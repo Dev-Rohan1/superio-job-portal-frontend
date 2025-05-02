1f884e3f045d6e48b6d7c9045cf5c658f55f26e3
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-05-02 17:40:34 +0600

    complete creation backend

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -4,7 +4,7 @@ import { Clock, MapPin, User } from "lucide-react";
 import moment from "moment";
 import React, { useContext, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
 import Footer from "../components/Footer";
 import JobCard from "../components/JobCard";
@@ -16,6 +16,8 @@ const ApplyJob = () => {
   const [jobData, setJobData] = useState(null);
   const [alreadyApplied, setAlreadyApplied] = useState(false);
   const [noSimilarJobs, setNoSimilarJobs] = useState(false);
+
+  const navigate = useNavigate();
 
   const { id } = useParams();
   const {
@@ -30,9 +32,11 @@ const ApplyJob = () => {
   const applyJob = async (jobId) => {
     try {
       if (!userData) {
+        navigate("/candidate-login");
         return toast.error("Please login to apply");
       }
       if (!userData?.resume) {
+        navigate("/applications");
         return toast.error("Please upload your resume");
       }
 
